869037f3a41327c2cef7a2b10da6e63e276e4163
zhangmingju <1417278284@qq.com>
2017-07-23 11:30:46 +0800

    fix install name

--- lib/generators/mo_record/install_generator.rb
+++ lib/generators/mo_record/install_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 require 'rails/generators/migration'
 
-module Audited
+module MoRecord
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
